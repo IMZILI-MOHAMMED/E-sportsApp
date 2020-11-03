a9176ecf2267b8f3c91099dfc3a17418e807cb53
Mohammed IMZILI <imzili.med@gmail.com>
2020-11-03 17:31:37 +0100

    teams list

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Teams() {
-  return (
-    <div className='reports'>
-      <h1>Teams</h1>
-    </div>
-  );
-}
+class Teams extends Component {
+  state = {
+    teams: []
+  }
 
-export default Teams;
+  componentDidMount() {
+    
+    fetch("https://api.pandascore.co/ow/teams?&token=ib5ahXVcsVhUwxLKEggXfrDx9-78AZEYD37rfNGsxeEhQDpM8Ic")
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ teams: data })
+    })
+    .catch(console.log)
+  }
+
+  render(){
+    return (
+      <div class="list">
+        <center><h1>Teams List</h1></center>
+        {this.state.teams.map((team) => (
+          <div class="card">
+            <div className="avatar">
+                          <img src={team.image_url} alt={team.name}/>
+                        </div>
+            <div class="card-body">
+              <h5 class="card-title">{team.name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{team.slug}</h6>
+              <p class="card-text">{team.acronym}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+   }
+    
+  }
+  
+  export default Teams;
